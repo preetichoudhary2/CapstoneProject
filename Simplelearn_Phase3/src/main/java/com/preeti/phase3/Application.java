35c6767da7d399f5a1e49a34cb1e29a73eca822f
preet.choudhary2 <preeti.choudhary2@vodafone.com>
2023-05-03 13:09:54 +0000

    updating project

---
+++ Simplelearn_Phase3/src/main/java/com/preeti/phase3/Application.java
@@ -0,0 +1,14 @@
+package com.preeti.phase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello preeti");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Simplelearn_Phase3/src/main/java/com/preeti/phase3/BuyRepo.java
@@ -0,0 +1,7 @@
+package com.preeti.phase3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyRepo extends JpaRepository<Buy, Integer>{
+
+}
